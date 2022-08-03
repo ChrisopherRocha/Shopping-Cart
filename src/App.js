9ec9a17e95d93d329ac049ac3d2ab0c8d6241088
albertrocha <rocharocharocha95@yahoo.com>
2022-08-02 20:28:38 -0400

    finished at 40:00

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Nav from './components/Nav.jsx';
 import Header from './components/Header';
 import Landing from './components/Landing.jsx';
+import Highlights from './components/Highlights.jsx';
+import Featured from './components/Featured.jsx';
+
 function App() {
   return (
     <div className=''>
       <Nav />
       <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faShoppingCart, faTimes)
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
